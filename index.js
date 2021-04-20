6ae271b48459341c6e7173e17ff551ce57c394b3
Tyler Bray <broncobray77@gmail.com>
2021-04-20 16:40:39 -0700

    changed folder structure and added more cats

--- index.js
+++ index.js
@@ -1,44 +1,68 @@
 var cats = [
   {
     name: "Bob",
-    bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus maxime et temporibus! Aliquam, voluptatem illo! Pariatur quisquam et saepe molestias.",
-    image: './cat1.jpeg',
+    bio: "Are we in the litter box? Because I'm diggin' you.",
+    image: './img/cat1.jpeg',
     like: Math.random() >= 0.5 ? 'True' : 'False'
   },
   {
     name: "Bill",
-    bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus maxime et temporibus! Aliquam, voluptatem illo! Pariatur quisquam et saepe molestias.",
-    image: './cat2.jpeg',
+    bio: "Are you feline the connection between us?",
+    image: './img/cat2.jpeg',
     like: Math.random() >= 0.5 ? 'True' : 'False'
   },
   {
     name: "Jason",
-    bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus maxime et temporibus! Aliquam, voluptatem illo! Pariatur quisquam et saepe molestias.",
-    image: './cat3.jpeg',
+    bio: "Can i spend all my 9 lives with you?",
+    image: './img/cat3.jpeg',
     like: Math.random() >= 0.5 ? 'True' : 'False'
   },
   {
     name: "Kyle",
-    bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus maxime et temporibus! Aliquam, voluptatem illo! Pariatur quisquam et saepe molestias.",
-    image: './cat4.jpeg',
+    bio: "Can you take me to the vet, because you just took my breath away.",
+    image: './img/cat4.jpeg',
     like: Math.random() >= 0.5 ? 'True' : 'False'
   },
   {
     name: "Loren",
-    bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus maxime et temporibus! Aliquam, voluptatem illo! Pariatur quisquam et saepe molestias.",
-    image: './cat5.jpeg',
+    bio: "I hope you have pet insurance, because I’m about to destroy your pussy.",
+    image: './img/cat5.jpeg',
     like: Math.random() >= 0.5 ? 'True' : 'False'
   },
   {
     name: "Joe",
-    bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus maxime et temporibus! Aliquam, voluptatem illo! Pariatur quisquam et saepe molestias.",
-    image: './cat6.jpeg',
+    bio: "How you feline? Cause you purrfect, I’m not kitten!",
+    image: './img/cat6.jpeg',
+    like: Math.random() >= 0.5 ? 'True' : 'False'
+  },
+  {
+    name: "Michael",
+    bio: "You must have been sleeping in the sun, because you are HOT.",
+    image: './img/cat7.jpeg',
+    like: Math.random() >= 0.5 ? 'True' : 'False'
+  },
+  {
+    name: "Joe",
+    bio: "You remind me off a leopard... or a tiger. Either way, I don't mind the claws",
+    image: './img/cat8.jpeg',
+    like: Math.random() >= 0.5 ? 'True' : 'False'
+  },
+  {
+    name: "Joe",
+    bio: "Meet me by the dead bird.",
+    image: './img/cat9.jpeg',
+    like: Math.random() >= 0.5 ? 'True' : 'False'
+  },
+  {
+    name: "Joe",
+    bio: "How you feline? Cause you purrfect, I’m not kitten!",
+    image: './img/cat10.jpeg',
     like: Math.random() >= 0.5 ? 'True' : 'False'
   },
   {
     name: "No cats in your area",
     bio: "You've seen all the single cats in your area! Check back later for more.",
-    image: './cat7.jpeg'
+    image: './img/end.jpeg'
   }
 ]
 
@@ -48,11 +72,12 @@ var disliked = []
 var count = 0
 
 //load first cat
-document.getElementsByClassName('card-img-top')[0].setAttribute('src', './cat1.jpeg')
+document.getElementsByClassName('card-img-top')[0].setAttribute('src', './img/cat1.jpeg')
 document.getElementsByClassName('card-title')[0].innerHTML = cats[0].name
 document.getElementsByClassName('card-text')[0].innerHTML = cats[0].bio
 
 function dislike(e){
+    console.log(cats[count].image)
   count++
   e.preventDefault()
   disliked.push(cats[count])
